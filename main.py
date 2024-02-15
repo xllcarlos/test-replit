1b885f569fbbf23b3a32caca3257a97dfb1bae79
xllcarlos <113157343+xllcarlos@users.noreply.github.com>
2024-02-15 00:05:09 +0000

    inserir coisas

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 print("Olá mundo!")
 print("Teste replit")
+
+a = int(input("insira um valor para a: "))
+b = int(input("insira um valor para b: "))
+
+soma = a + b
+
+print("A soma de a + b é: ", soma)
